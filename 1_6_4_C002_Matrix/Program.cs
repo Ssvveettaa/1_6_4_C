e37645d29a1cf39e6cdb3a8b73fe1454ce87b82a
Svetlana Panina <paninasi@inbox.ru>
2023-08-24 20:20:13 +0300

    1_6_4_C002_Matrix

---
+++ 1_6_4_C002_Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿// 1_6_4_C002_Matrix:
+
+void FillMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1, 10); // [1; 10) = [1; 9]
+        }
+    }
+}
+
+void OutputMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // i – для строк. В "GetLength", 0 – строки.
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // j – для столбцов. В "GetLength", 1 – столбцы.
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+//           GetLength: 0  1 – порядковый номер размерности массива.
+int[,] matrix = new int[3, 4];
+OutputMatrix(matrix);
+Console.WriteLine();
+
+FillMatrix(matrix);
+OutputMatrix(matrix);
