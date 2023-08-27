c16b9aa111c4d28532340a6165e209ae4e0cb5cb
Svetlana Panina <paninasi@inbox.ru>
2023-08-28 00:43:46 +0300

    1_6_4_C004_Factorial

---
+++ 1_6_4_C004_Factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿// 1_6_4_C004_Factorial:
+
+double Factorial(int n)
+{
+    // 0! = 1
+    // 1! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i, 2}! = {Factorial(i)}"); // 3! = 1 * 2 * 3 = 6
+}
